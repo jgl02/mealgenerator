38304857be6db20f43d2ed2a69d88217e49a8105
jgl02 <60861519+jgl02@users.noreply.github.com>
2020-09-13 13:08:18 -0400

    Update breakfast.js

--- breakfast.js
+++ breakfast.js
@@ -1,18 +1,18 @@
   export const breakfastlist =
   [
     {
-      idMeal: '1'
-      strMeal: 'Yogurt Parfait'
+      "idMeal": '1',
+      "strMeal": 'Yogurt Parfait',
       strLink: 'https://www.tasteofhome.com/recipes/rise-and-shine-parfait/'
     },
     {
-      idMeal: '2'
-      strMeal: 'Avacado Toast & Eggs'
-      strLink: 'https://www.favfamilyrecipes.com/open-face-avocado-egg-sandwich/#wprm-recipe-container-19935'
+      "idMeal": '2',
+      "strMeal": 'Avacado Toast & Eggs',
+      "strLink": 'https://www.favfamilyrecipes.com/open-face-avocado-egg-sandwich/#wprm-recipe-container-19935'
     },  
     {
-      idMeal: '3'
-      strMeal: 'Oatmeal and Fruit'
-      strLink: 'https://healthyfitnessmeals.com/fruit-and-oatmeal-breakfast-bowl/'
+      "idMeal": '3',
+      "strMeal": 'Oatmeal and Fruit',
+      "strLink": 'https://healthyfitnessmeals.com/fruit-and-oatmeal-breakfast-bowl/'
     }
    ]
