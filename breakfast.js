f0ce4bc9371fd1c6b7b12daee410adfb34ac39f7
jgl02 <60861519+jgl02@users.noreply.github.com>
2020-09-13 12:41:21 -0400

    Create breakfast.js

---
+++ breakfast.js
@@ -0,0 +1,19 @@
+{
+  breakfast: [
+    {
+      idMeal: '1'
+      strMeal: 'Yogurt Parfait'
+      strLink: 'https://www.tasteofhome.com/recipes/rise-and-shine-parfait/'
+    },
+    {
+      idMeal: '2'
+      strMeal: 'Avacado Toast & Eggs'
+      strLink: 'https://www.favfamilyrecipes.com/open-face-avocado-egg-sandwich/#wprm-recipe-container-19935'
+    },  
+    {
+      idMeal: '3'
+      strMeal: 'Oatmeal and Fruit'
+      strLink: 'https://healthyfitnessmeals.com/fruit-and-oatmeal-breakfast-bowl/'
+    }
+   ];
+}
