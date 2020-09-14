9fc312fabf7270ea34da4275f55c3a4cd2436178
jgl02 <60861519+jgl02@users.noreply.github.com>
2020-09-13 22:31:19 -0400

    Update breakfast.js

--- breakfast.js
+++ breakfast.js
@@ -1,17 +1,17 @@
 var breakfastlist = [
-    {
-      idMeal: '1',
-      strMeal: 'Yogurt Parfait',
-      strLink: 'https://www.tasteofhome.com/recipes/rise-and-shine-parfait/'
-    },
-    {
-      idMeal: '2',
-      strMeal: 'Avacado Toast & Eggs',
-      strLink: 'https://www.favfamilyrecipes.com/open-face-avocado-egg-sandwich/#wprm-recipe-container-19935'
-    },  
-    {
-      idMeal: '3',
-      strMeal: 'Oatmeal and Fruit',
-      strLink: 'https://healthyfitnessmeals.com/fruit-and-oatmeal-breakfast-bowl/'
-    },
-   ];
+      {
+        idMeal: 1,
+        mealName: 'Yogurt Parfait',
+        mealLink: 'https://www.tasteofhome.com/recipes/rise-and-shine-parfait/'
+      },
+      {
+      idMeal: 2,
+      mealName: 'Avocado Toast & Eggs',
+      mealLink: 'https://www.favfamilyrecipes.com/open-face-avocado-egg-sandwich/#wprm-recipe-container-19935'
+      },  
+      {
+        idMeal: 3,
+        mealName: 'Oatmeal and Fruit',
+        mealLink: 'https://healthyfitnessmeals.com/fruit-and-oatmeal-breakfast-bowl/'
+      },
+     ]
